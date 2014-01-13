64e4feb335edcf997e1ee314987498eb51797702
prih <prihmail@gmail.com>
2014-01-13 19:34:53 +0200

    add history, route, ejs

---
+++ js/jfw.route.js
@@ -0,0 +1,45 @@
+/**
+	JavaScript Framework Hash Routing
+
+	@version 0.0.1
+	@author andrey prih <prihmail@gmail.com>
+*/
+(function(){
+	if (!fw) {
+		console.error('you must first load jfw.base');
+		return;
+	}
+
+	var route_rules = {};
+
+	var route = new fw.Map({
+		add: function(rule, handler) {
+			route_rules[rule] = {
+				reg: new RegExp(rule),
+				handler: handler
+			};
+		},
+		remove: function(rule) {
+			delete route_rules[rule];
+		}
+	});
+
+	route.attr('hash', window.location.hash);
+
+	window.onpopstate = function() {
+		route.attr('hash', window.location.hash);
+	};
+
+	route.bind('hash', function(e){
+		for (var i in route_rules) {
+			if(route_rules[i].reg.test(this.hash.replace(/^#/, ''))) {
+				var param = this.hash.match(i);
+				param = Array.prototype.slice.call(param, 1, param.length);
+				route_rules[i].handler.call(this, param);
+				break;
+			}
+		}
+	});
+
+	fw.route = route;
+})(window);
