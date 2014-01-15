be746487d7370c6ab813c7c333b0d501633289c8
prih <prihmail@gmail.com>
2014-01-15 13:14:55 +0200

    externs for CC, min.js

---
+++ js/externs.js
@@ -0,0 +1,35 @@
+window.fw = {};
+var fw = {};
+fw.utils = {};
+fw.Construct;
+fw.Construct.extend;
+fw.Map;
+fw.Map.extend;
+fw.List;
+fw.List.extend;
+fw.compute;
+
+var map_proto = {};
+map_proto.attr;
+map_proto.each;
+map_proto.removeAttr;
+map_proto.bind;
+map_proto.unbind;
+map_proto.trigger;
+
+var param = {};
+param.init;
+var default_param = {};
+default_param.init;
+
+var list_proto = {};
+list_proto.push;
+list_proto.pop;
+list_proto.shift;
+list_proto.unshift;
+list_proto.slice;
+list_proto.splice;
+list_proto.indexOf;
+list_proto.join;
+list_proto.forEach;
+list_proto.concat;
